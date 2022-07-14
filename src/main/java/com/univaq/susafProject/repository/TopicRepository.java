2d20a265d86b2df25214a113c8e6e749053d90f9
essietom <essietom@gmail.com>
2022-07-14 09:54:48 +0200

    remove redundant repositories

--- src/main/java/com/univaq/susafProject/repository/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.univaq.susafProject.repository;
-
-//import com.univaq.susafProject.model.Topic;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface TopicRepository extends MongoRepository<Topic, String> {
-//}
